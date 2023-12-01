c93245ce6b311ea7b1d1917d8cc78b6065b3d72a
Atlas16A <thegoldenatlaspass@gmail.com>
2023-12-01 15:47:34 -0500

    Fixed edgeless nodes. Cleaned up and comment code.

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,56 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use eframe::{App, CreationContext};
+use eframe::{egui, App, CreationContext};
 use egui::Context;
 use egui_graphs::{DefaultEdgeShape, Graph, GraphView, SettingsInteraction, SettingsNavigation};
-
-use eframe::egui;
 use petgraph::{
     stable_graph::{DefaultIx, NodeIndex, StableGraph},
     Directed,
 };
 
-mod node;
+//eframe and egui styling
 mod theme;
-use node::NodeShapeAnimated;
+//Edge Display code
 mod edge;
+//Node Display code
+mod node;
+use node::NodeShapeAnimated;
 
 pub struct Lsegui {
-    //Graph<(), (), Directed, DefaultIx, NodeShapeAnimated>
+    //The graph that will be displayed
     g: Graph<(), (), Directed, DefaultIx, NodeShapeAnimated>,
+    //The user input string that will be used to create the graph
     input_string: String,
+    //Boolean to display the graph once the user has entered a phrase
     graph_show: bool,
 }
 
 impl Lsegui {
     fn new(cc: &CreationContext<'_>) -> Self {
-        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
-        // Restore app state using cc.storage (requires the "persistence" feature).
-        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
-        // for e.g. egui::PaintCallback.
-        let g = init_graph();
+        // Initialize the graph
+        let g = StableGraph::new();
+        //Apply the style from the theme module
         let style = theme::style();
         cc.egui_ctx.set_style(style);
+
         Self {
+            //By default the graph is empty and not displayed
             g: Graph::from(&g),
             input_string: String::new(),
             graph_show: false,
         }
     }
-
+    //Reset the graph and its metadata to ensure that the graph is ready for the next input
     fn reset_graph(&mut self, ui: &mut egui::Ui) {
-        let g = init_graph();
+        let g = StableGraph::new();
 
         self.g = Graph::from(&g);
 
         GraphView::<(), (), Directed, DefaultIx>::reset_metadata(ui);
     }
 
-    //if char_node_pairs contains a char value that is in connections create an edge between the nodeIndex of the current char and the nodeIndex of the char in connections
+    //Check if the char in the phrase is connected to any other char in the phrase and add an edge between them
+    //Some nodes may not get any connections if the current word does not have any letters for the current character to connect to
     fn connections_check(
         &mut self,
         connections: &str,
@@ -55,21 +59,31 @@ impl Lsegui {
         char_node_pairs: &mut [(char, NodeIndex<u32>)],
         k: usize,
     ) {
-        for target_char in connections.chars() {
+        connections.chars().for_each(|target_char| {
             let target_indices: Vec<NodeIndex<u32>> = char_node_pairs
                 .iter()
                 .filter(|(c, _)| *c == target_char)
                 .map(|(_, index)| *index)
                 .collect();
 
-            for target_index in target_indices {
+            target_indices.into_iter().for_each(|target_index| {
                 g.add_edge(node_indices[k], target_index, ());
-            }
-        }
+            });
+        });
     }
 
+    //Create the graph from the user input
+    //Take in the phrase and convert it to uppercase
+    //Split the phrase into words
+    //For each word in the phrase:
+    //  Add a node for each letter in the word
+    //  Add edges between the nodes for each letter in the word
+    //  Set the label for each node to the letter in the word at the same index as the node
+    //  Position each node along a circle for each word in phrase
+    //      with the first circle being at the center of the canvas and
+    //      the next circle being to the bottom of the first circle
     fn node_creation(&mut self, phrase: &str) {
-        let mut g = init_graph();
+        let mut g = StableGraph::new();
 
         let binding = phrase.to_uppercase();
         let binding = binding.split_whitespace();
@@ -81,18 +95,18 @@ impl Lsegui {
             println!("Phrase: {}", &word);
             //Add Node for each letter in phrase
             let mut node_indices: Vec<NodeIndex<u32>> = vec![];
-            for _char in word.chars() {
+            word.chars().for_each(|_char| {
                 node_indices.push(g.add_node(()));
-            }
+            });
 
             //put the chars from phrase into tuple pairs with the node idex of the char
             let mut char_node_pairs: Vec<(char, NodeIndex<u32>)> = vec![];
-            for (i, char) in word.char_indices() {
+            word.char_indices().for_each(|(i, char)| {
                 char_node_pairs.push((char, node_indices[i]));
-            }
+            });
 
             //Add edges for each letter in phrase
-            for (k, char) in word.char_indices() {
+            word.char_indices().for_each(|(k, char)| {
                 match char {
                     'A' | 'a' => (),
                     'B' | 'b' => self.connections_check(
@@ -342,12 +356,21 @@ impl Lsegui {
                             k,
                         );
                     }
-                    _ => {
-                        // Handle the case when char is not any of the above
-                        continue;
-                    }
+                    _ => (),
                 };
-            }
+            });
+
+            //Deal with nodes that do not have any connections
+            //Had to seperate from the previous loop because need to have all the edges added before checking for nodes with no connections
+            word.char_indices().for_each(|(k, char)| {
+                node_clean_up(
+                    &mut g,
+                    node_indices.clone(),
+                    k,
+                    char_node_pairs.clone(),
+                    char,
+                );
+            });
         });
 
         let node_indices = g.node_indices().collect::<Vec<_>>();
@@ -360,12 +383,12 @@ impl Lsegui {
             .to_uppercase();
 
         //Set Label for each node, Label is the char in phrase at the same index as the node
-        for (i, char) in phrase.char_indices() {
+        phrase.char_indices().for_each(|(i, char)| {
             self.g
                 .node_mut(node_indices[i])
                 .unwrap()
                 .set_label(char.to_string());
-        }
+        });
 
         self.layout_nodes(binding, node_indices);
     }
@@ -387,9 +410,9 @@ impl Lsegui {
             let mut angle: f32 = -90.0;
             let angle_increment = 360.0 / word.len() as f32;
 
-            for (i, mut node_index) in node_indices.iter().enumerate() {
+            node_indices.iter().enumerate().for_each(|(i, _)| {
                 if i < word.len() {
-                    node_index = &node_indices[i + offset];
+                    let node_index = &node_indices[i + offset];
                     let x = center_x + angle.to_radians().cos() * radius;
                     let y = center_y + angle.to_radians().sin() * radius;
                     self.g
@@ -397,13 +420,46 @@ impl Lsegui {
                         .unwrap()
                         .set_location(egui::Pos2::new(x, y));
                     angle += angle_increment;
-                } else {
-                    break;
                 }
-            }
+            });
             offset += word.len();
             center_y += vertical_offset;
         });
+    }
+}
+
+fn node_clean_up(
+    g: &mut StableGraph<(), ()>,
+    node_indices: Vec<NodeIndex>,
+    k: usize,
+    char_node_pairs: Vec<(char, NodeIndex)>,
+    char: char,
+) {
+    //If the current node does not have any incoming or outgoing edges
+    if g.neighbors_directed(node_indices[k], petgraph::Direction::Outgoing)
+        .count()
+        == 0
+        && g.neighbors_directed(node_indices[k], petgraph::Direction::Incoming)
+            .count()
+            == 0
+    {
+        //Out of the nodes in the current word,
+        //connect the current node to the node
+        //representing the character closest to the current character on the alphabet
+        let mut closest_index = 0;
+        let mut closest_distance = 26;
+        char_node_pairs
+            .iter()
+            .filter(|(c, _)| *c != char)
+            .enumerate()
+            .for_each(|(i, (c, _))| {
+                let distance = (char as i32 - *c as i32).abs();
+                if distance < closest_distance {
+                    closest_distance = distance;
+                    closest_index = i;
+                }
+            });
+        g.add_edge(node_indices[k], node_indices[closest_index], ());
     }
 }
 
@@ -411,16 +467,19 @@ impl App for Lsegui {
     fn update(&mut self, ctx: &Context, _: &mut eframe::Frame) {
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             ui.horizontal(|ui| {
-                //take sentence input, with font size 20
                 ui.label("Enter a Phrase:");
+                //Take in user text input
                 let re = ui.text_edit_singleline(&mut self.input_string);
                 if re.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
                     println!("Input: {}", self.input_string);
+                    //Clear the current graph just in case
                     self.reset_graph(ui);
+                    //Convert the input to a string slice
                     let phrase = self.input_string.clone();
                     let phrase = phrase.as_str();
-
+                    //Create the graph from the input
                     self.node_creation(phrase);
+                    //Display the graph
                     self.graph_show = true;
                 }
             });
@@ -447,10 +506,6 @@ impl App for Lsegui {
             }
         });
     }
-}
-
-fn init_graph() -> StableGraph<(), ()> {
-    StableGraph::new()
 }
 
 // When compiling natively:
